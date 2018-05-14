3dc1e80420458d2e35194568c363de6a8915e500
yoichi-watanabe <chip.star1120@gmail.com>
2018-05-15 02:35:26 +0900

    webdbpress vol.104いきなりiPhoneアプリ開発の1, 2章を実施

---
+++ TestApp/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  TestApp
+//
+//  Created by Yoichi Watanabe on 2018/05/15.
+//  Copyright © 2018年 Yoichi Watanabe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -13,13 +13,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // ボタンの背景色を黒色に設定
+        button.backgroundColor = .black
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var button: UIButton!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        print("Hello, world!")
+        
+        // Main.storyboardからSecondViewControllerを取得
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let storyboardID = "SecondViewController"
+        let secondVC = storyboard.instantiateViewController(withIdentifier: storyboardID)
+        
+        // 別の画面へ遷移したことがわかりやすいよう、画面の背景色を青に変更
+        secondVC.view.backgroundColor = .blue
+        
+        // SecondViewControllerへ画面遷移
+        self.present(secondVC, animated: true)
+    }
 }
 
